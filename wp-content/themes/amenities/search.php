7e8667be7bc1af68db85e12a7bc89a81c7ff5d6c
Lucas <llellismachado@hotmail.com>
2016-06-14 18:11:22 -0300

    VersionWithCustomPostType
    Versão com post type customizado e campos personalizados

--- wp-content/themes/amenities/search.php
+++ wp-content/themes/amenities/search.php
@@ -1,17 +1,24 @@
 <?php get_header(); ?>
     <div id="conteudo">
         <div id="artigos">
-            <div class="artigo">
-                <h2>Titulo do artigo 1</h2>
-                <p>Postado por administrador em 16/01/2012</p>
-                <p>Conteudo do artigo</p>
-            </div>
+         
+            <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+                <div class="artigo">
+                    <h1><a href="<?php the_permalink() ?>"><?php the_title(); ?></a></h1>
+                    <p>Postado por <?php the_author() ?> em <?php the_time('d/M/Y') ?> - <?php comments_popup_link('Sem Comentários', '1 Comentário', '% Comentários', 'comments-link', ''); ?> <?php edit_post_link('(Editar)'); ?></p>
+                    <p><?php the_content(); ?></p>
+                </div>
+                 
+                <?php comments_template(); ?>
+                     
+            <?php endwhile; else: ?>
+                <div class="artigo">
+                    <h2>Nada Encontrado</h2>
+                    <p>Erro 404</p>
+                    <p>Lamentamos mas não foram encontrados artigos.</p>
+                </div>            
+            <?php endif; ?>
              
-            <div class="artigo">
-                <h2>Titulo do artigo 2</h2>
-                <p>Postado por administrador em 16/01/2012</p>
-                <p>Conteudo do artigo</p>
-            </div>
         </div>
          
         <?php get_sidebar(); ?>
